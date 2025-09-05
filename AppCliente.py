941db5886513661c9c920116b4b0d33a0af5d147
Diogo Rangel <diogo.rangelgoncalves09@gmail.com>
2025-09-05 10:55:13 -0300

    Update AppCliente.py

--- AppCliente.py
+++ AppCliente.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
             continue
         #ESCOLHAS DO MENU:
         if n == 1:
-            print("---- CADASTRO DO PACIENTE ----\n")
+            print("---- CADASTRO DO PACIENTE ----")
             nome = input("Infome o nome do paciente: ")
             p = int(input("O paciente é prioridade? [1 -Sim/ 2- Não] "))
             if p == 1:
@@ -21,13 +21,13 @@ if __name__ == "__main__":
                 prioridade = False
             fila_pacientes.add(nome, prioridade)
         elif n == 2:
-            print("---- ATENDIMENTO AO PACIENTE ----\n")
-            fila_pacientes.atender() #Atende na ordem que foi adicionado com as regras estabelecidas
+            print("---- ATENDIMENTO AO PACIENTE ----")
+            print(f"Paciente atendido: {fila_pacientes.atender()}") #Atende na ordem que foi adicionado com as regras estabelecidas
         elif n == 3:
-            print("---- LISTA DE PACIENTES ----\n")
+            print("---- LISTA DE PACIENTES ----")
             print(fila_pacientes.listar())
         elif n == 4:
-            if fila_pacientes.tamanho == 0:
+            if fila_pacientes.tam1 == 0 and fila_pacientes.tam2 == 0:
                 break
             else:
                 print("Existem pacientes para serem atendidos.")
